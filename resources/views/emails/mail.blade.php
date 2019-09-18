20b0862645eaf44cf542aec5be16900d2e00544b
yuguanthor <luckboy5566@hotmail.com>
2019-09-18 18:01:56 +0800

    YG

--- resources/views/emails/mail.blade.php
+++ resources/views/emails/mail.blade.php
@@ -3,6 +3,6 @@
 
 @if( isset($unsubscribe_link) )
 <hr>
-If you do not want to receive this kind of email.<br>
-You may <a href="{{$unsubscribe_link}}">Unsubscribe</a> it.
+If you do not wish to receive newsletter from Vinstella in future<br>
+you may <a href="{{$unsubscribe_link}}">unsubscribe</a> it.
 @endif
